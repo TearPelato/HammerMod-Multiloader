d59daba4807d1626f437b05c9bf2772ed1e2fdd8
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-04-08 17:04:13 +0200

    Ported ToolTiers to work on 26.1.1

--- common/src/main/java/net/tier1234/hammermod/registries/ModToolTiers.java
+++ common/src/main/java/net/tier1234/hammermod/registries/ModToolTiers.java
@@ -1,15 +1,15 @@
 package net.tier1234.hammermod.registries;
 
+import net.minecraft.world.item.ToolMaterial;
+import net.tier1234.hammermod.util.ModTags;
+
 public class ModToolTiers {
-/*
-    public static final ToolMaterial COPPER = new ToolMaterial() {
-
-    };
-
-    public static final ToolMaterial AMETHYST = new ToolMaterial() {
-
-    };*/
+    /**
+     * No longer needed the COPPER material cause it's vanilla since 1.21.9*/
+    public static final ToolMaterial AMETHYST = new ToolMaterial(ModTags.Blocks.INCORRECT_FOR_AMETHYST_TOOL,
+            150, 2f, 2f, 25, ModTags.Items.AMETHYST_REPAIRABLE );
 }
+
 
 
 
